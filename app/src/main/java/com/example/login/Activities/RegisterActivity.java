a71e70f420b5c0ba011f761ee900ba2267fc52ae
marius <mapo1@teleco.upv.es>
2019-06-11 18:48:39 +0200

    login

---
+++ app/src/main/java/com/example/login/Activities/RegisterActivity.java
@@ -0,0 +1,35 @@
+package com.example.login.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+
+import com.example.login.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    private EditText userEmail,userPassword,userPAssword2,userName;
+    private ProgressBar loadingProgress;
+    private Button regBtn;
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        userEmail = findViewById(R.id.regMail);
+        userPassword = findViewById(R.id.regPass);
+        userPAssword2 = findViewById(R.id.regPass2);
+        userName = findViewById(R.id.regName);
+        loadingProgress = findViewById(R.id.regProgressBar);
+        regBtn = findViewById(R.id.regBtn);
+        loadingProgress.setVisibility(View.INVISIBLE);
+
+        mAuth = FirebaseAuth.getInstance();
+
+    }
+}
